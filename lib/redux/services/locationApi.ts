2a2c6e80c9e8f86b7e11147cd4dd6b5a5001c913
Zülal Nebin <zllnbn1997@gmail.com>
2023-12-23 20:40:13 +0300

    fix filtering and pagination on location api

--- lib/redux/services/locationApi.ts
+++ lib/redux/services/locationApi.ts
@@ -1,8 +1,18 @@
 // Import the createApi and fetchBaseQuery functions from the RTK Query library
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { Locations } from "@/types/locations";
-import { Characters } from "@/types/characters";
+import {
+  FetchBaseQueryError,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { Location, Locations } from "@/types/locations";
+import { Character, CharacterDetail, Characters } from "@/types/characters";
 import { Status } from "@/types/general";
+
+const paginateData = (data: any, page: number, itemsPerPage: number) => {
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  return data.slice(startIndex, endIndex);
+};
 
 export const locationsAPI = createApi({
   reducerPath: "locationsAPI",
@@ -11,17 +21,93 @@ export const locationsAPI = createApi({
     baseUrl: "https://rickandmortyapi.com/api",
   }),
   endpoints: (builder) => ({
-    getLocationsByPage: builder.query<Locations, number | void>({
-      query: (page = 1) => `location?page=${page}`,
+    getLocationsByPage: builder.query<Locations, number>({
+      // query: (page = 1) => `location/?page=${page}`,
+      queryFn: async (arg, _queryApi, _extraOptions, fetchWithBQ) => {
+        let endpoint = "location";
+
+        const res = await fetchWithBQ(endpoint);
+
+        const locations = res.data as Locations;
+
+        const pageArray = Array.from(
+          { length: locations.info.pages },
+          (_, index) => index + 1
+        );
+
+        let allLocations: Location[] = [];
+        await Promise.all(
+          pageArray.map(async (pageNumber) => {
+            const pageres = await fetchWithBQ(
+              `${endpoint}/?page=${pageNumber}`
+            );
+
+            const pagedata = pageres.data as Locations;
+
+            allLocations.push(...pagedata.results);
+          })
+        );
+
+        const data = paginateData(allLocations, arg, 3);
+
+        locations.info.pages = 3;
+        locations.info.next = null;
+        locations.info.prev = null;
+
+        locations.results = data;
+
+        return { data: locations as Locations };
+      },
     }),
     getCharactersByLocation: builder.query<
       Characters,
       { id: number; status: string; page: number }
     >({
-      query: ({ id, page, status = "all" }) =>
-        status === "all"
-          ? `character?location=${id}&page=${page}`
-          : `character?location=${id}&status=${status}&page=${page}`,
+      queryFn: async (arg, _queryApi, _extraOptions, fetchWithBQ) => {
+        let endpoint = "character";
+
+        if (arg.status !== "all")
+          endpoint = endpoint + `/?status=${arg.status}`;
+
+        const res = await fetchWithBQ(endpoint);
+
+        const characters = res.data as Characters;
+
+        const pageArray = Array.from(
+          { length: characters.info.pages },
+          (_, index) => index + 1
+        );
+
+        let allCharacters: Character[] = [];
+        await Promise.all(
+          pageArray.map(async (pageNumber) => {
+            const query = `${endpoint}${
+              arg.status === "all" ? "/?" : "&"
+            }page=${pageNumber}`;
+            const pageres = await fetchWithBQ(query);
+            const pagedata = pageres.data as Characters;
+            const charactersByLocations = pagedata.results.filter(
+              (character) => {
+                return (
+                  Number(character.location.url.split("/").slice(-1)) === arg.id
+                );
+              }
+            );
+            allCharacters.push(...charactersByLocations);
+          })
+        );
+
+        const data = paginateData(allCharacters, arg.page, 3);
+
+        characters.info.count = allCharacters.length;
+        characters.info.pages = 3;
+        characters.info.next = null;
+        characters.info.prev = null;
+
+        characters.results = data;
+
+        return { data: characters as Characters };
+      },
     }),
   }),
 });
